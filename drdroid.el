870548e118b5ead6ac9b4c57d96e0d8f845d7073
hwiorn <hwiorn@gmail.com>
2012-02-02 02:20:02 +0900

    uninstall package & device list 선택 & ddms 시작 등등 추가

--- drdroid.el
+++ drdroid.el
@@ -1,16 +1,226 @@
-(defvar selected-device "")
 
-(defun adb-command (command)
-  (shell-command-to-string (format "adb \"%s\"" command)))
-(defun drdroid-select-device ()
-  (let (found t)
-       (device-list ())
+(eval-when-compile (require 'cl))
+
+(defvar drdroid-selected-device ())
+(defvar drdroid-device-list ())
+(defvar drdroid-sdk-path ())
+
+;; Android ADB command
+(defun adb-devices ()
+  (shell-command-to-string "adb devices"))
+
+(defun adb-command (device-id command)
+  (shell-command-to-string (format "adb -s \"%s\" %s" device-id command)))
+
+(defun adb-shell (device-id command)
+  (shell-command-to-string (format "adb -s \"%s\" shell \"%s\"" device-id command)))
+
+;; Dr.Droid Functions
+
+;; device 목록을 갱신
+;; TODO: avd 목록도 같이 가지고 있는 것도 괜찮을 듯
+(defun drdroid-get-device-list ()
+  (let ((found t)
+	(device-list ()))
     (with-temp-buffer
-      (insert (adb-command "devices"))
+      (insert (adb-devices))
       (goto-char (point-min))
       (while found
 	(forward-line)
 	(setq found (search-forward "\t" nil t))
 	(when found
-	  (setq (append device-list (substring (beginning-of-line) (point-min)))))))
-    device-list))
+	  (let* ((founded-point (progn (backward-char)
+				       (point)))
+		 (beg-point (progn (beginning-of-line)
+				   (point)))
+		 (device-id (buffer-substring beg-point founded-point)))
+	    (setq device-list (append device-list (list device-id)))))))
+    (setq drdroid-device-list device-list)
+    (when (equal (safe-length device-list) 1)
+      (setq drdroid-selected-device (car device-list)))))
+
+;; 선택한 device가 갱신된 device-list에 존재하는지 검사
+;; 없으면 선택 device를 초기화
+(defun drdroid-check-valid-device-id ()
+  (drdroid-get-device-list)
+  (when (null (member drdroid-selected-device drdroid-device-list))
+	      (setq drdroid-selected-device ())))
+
+;; device 를 하나 선택
+(defun drdroid-select-device ()
+  (interactive)
+  (drdroid-check-valid-device-id)
+
+  (if drdroid-device-list
+      (let ((init-value (if (member drdroid-selected-device
+				    drdroid-device-list)
+			    drdroid-selected-device
+			  (car drdroid-device-list))))
+	(completing-read "select device : "
+			 drdroid-device-list
+			 nil
+			 0
+			 init-value
+			 'drdroid-selected-device)
+	(message "device [%s] selected" drdroid-selected-device))
+    (message "device(s) not founded")))
+
+;; 현재 선택된 것이 없을 경우, 다시 선택하도록 하거나 그대로 둠.
+(defun drdroid-check-device-or-re-select ()
+  (drdroid-check-valid-device-id)
+  (when (null drdroid-selected-device)
+    (drdroid-select-device))
+  drdroid-selected-device)
+
+;; 태그
+(defun drdroid-xml-get-attribute (attribute)
+  (let ((beg-attr (format "%s=\"" attribute))
+	(end-attr "\"")
+	(beg-point 0)
+	(end-point 0))
+    (search-forward beg-attr nil t)
+    (setq beg-point (point))
+    (search-forward end-attr nil t)
+    (backward-char (length end-attr))
+    (setq end-point (point))
+    (buffer-substring beg-point end-point)))
+
+;; android package 목록을 갱신
+(defun drdroid-get-package-list ()
+  (when (drdroid-check-device-or-re-select)
+    (let ((found t)
+	  (package-list ()))
+      (with-temp-buffer
+	(insert (adb-shell drdroid-selected-device "cat /data/system/packages.xml"))
+	(goto-char (point-min))
+	(while found
+	  (setq found (search-forward "<package " nil t))
+	  (when found
+	    (let* ((package-name (drdroid-xml-get-attribute "name"))
+		   (package-apk (drdroid-xml-get-attribute "codePath"))
+		   (package-version (drdroid-xml-get-attribute "version")))
+	      (setq package-list (append package-list
+					 ;; (list (format "%s(v%s) - %s"
+					 ;; 	       package-name
+					 ;; 	       package-version
+					 ;; 	       package-apk))
+					 (list package-name)
+					 ))))))
+      package-list)))
+
+;; package 선택 목록을 만들고 선택하게 되면 지운다
+(defun drdroid-uninstall-app (&rest package)
+  (interactive)
+  (when (drdroid-check-device-or-re-select)  
+    (let* ((package-list (drdroid-get-package-list))
+	   (package-selected ())
+	   (init-package (if package
+				 package
+			       (if (null package-list)
+				   ""
+				 (car package-list)))))
+      (when package-list
+	(completing-read "select package : "
+			 package-list
+			 nil
+			 0
+			 init-package
+			 'package-selected)
+	(when (and (listp package-selected)
+	       (y-or-n-p (format "do you really want to uninstall this package[%s] " (car package-selected))))
+	  (message (adb-command drdroid-selected-device (format "uninstall \"%s\"" (car package-selected))))) 
+      ))))
+
+;; android-mode support tool
+;; 지원해야 할 목록들
+;;
+;; android-ant 	android-ant-clean
+;; android-ant-compile 	android-ant-install
+;; android-ant-uninstall 	android-debug-activity
+;; android-emulate 	android-install-app
+;; android-jdb 	android-logcat
+;; android-logcat-find-file 	android-logcat-find-file-mouse
+;; android-mode 	android-start-activity
+;; android-start-ddms 	android-start-emulator
+;; android-uninstall-app
+
+(defvar drdroid-exclusive-processes ())
+(defun drdroid-start-exclusive-command (name command &rest args)
+  (and (not (find (intern name) drdroid-exclusive-processes))
+       (set-process-sentinel (apply 'start-process-shell-command name name command args)
+                             (lambda (proc msg)
+                               (when (memq (process-status proc) '(exit signal))
+                                 (setq drdroid-exclusive-processes
+                                       (delete (intern (process-name proc))
+                                               drdroid-exclusive-processes)))))
+       (setq drdroid-exclusive-processes (cons (intern name)
+                                               drdroid-exclusive-processes))))
+
+;;project 관련
+;; android tool에 update project 명령은 뭐지?
+(defun drdroid-create-project ())
+(defun drdroid-create-test-project ())
+(defun drdroid-create-lib-project ())
+(defun drdroid-create-identity ())
+
+;; tool 관련
+;; sdk path를 임의로 지정할 수 있도록 설정
+(defun drdroid-set-sdk-path ())
+
+;; droiddraw의 path를 임의로 지정할 수 있도록 설정
+(defun drdroid-set-droiddraw-path ())
+
+(defun drdroid-start-ddms ()
+  (interactive)
+  "Launch Dalvik Debug Monitor Service tool."
+  (interactive)
+  (unless (drdroid-start-exclusive-command "*android-ddms*" "ddms")
+    (message "ddms already running")))
+
+(defun drdroid-start-avd-manager ()
+  (interactive)
+  "Launch Android Virtual Device Manager tool."
+  (interactive)
+  (unless (drdroid-start-exclusive-command "*android-avd-manager*" "android avd")
+    (message "AVD Manager already running")))
+
+(defun drdroid-start-sdk-manager ()
+  (interactive)
+  "Launch SDK Manager tool."
+  (interactive)
+  (unless (drdroid-start-exclusive-command "*android-sdk-manager*" "android sdk")
+    (message "SDK Manager already running")))
+
+
+(defun drdroid-start-android-lint ())
+
+(defun drdroid-start-ninepatch ()
+  (interactive)
+  "Launch Draw 9 Patch tool."
+  (interactive)
+  (unless (drdroid-start-exclusive-command "*android-sdk-manager*" "draw9patch")
+    (message "Draw9Patch already running")))
+
+
+;;menu
+(defun drdroid-menu)
+(defun drdroid-minor-keymap ()
+  (let ((map (make-sparse-keymap)))
+    (easy-menu-define drdroid-menu map
+      "Dr.Droid Menu"
+      '("Dr.Droid - v1.0.0.0"
+	"----"
+	["ddms" drdroid-start-ddms
+	 :help "launch ddms"]
+	["AVD Manager" drdroid-start-avd-manager
+	 :help "launch avd manager"]
+	["SDK Manager" drdroid-start-sdk-manager
+	 :help "launch sdk manager"]
+	"----"
+	))
+    ;;TODO: define-key
+    map))
+
+;;현재 임시 디렉토리는 temporary-file-directory 변수 참조
+;;windows에서 에러날때가 있긴 한가봄
+
